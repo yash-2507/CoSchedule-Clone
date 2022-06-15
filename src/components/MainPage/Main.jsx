94e65ca998452109abdbbab5d61c98c1bbc7837b
yash-2507 <99003521+yash-2507@users.noreply.github.com>
2022-06-15 08:43:11 +0530

    some modification

--- src/components/MainPage/Main.jsx
+++ src/components/MainPage/Main.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import CrossSvg from "../SVG/Cross";
 import styles from "./Main.module.css";
 
 export default function Main() {
-    return <div>Main</div>;
+    return (
+        <div className={styles.parent}>
+            <div className={styles.Main_parent_Notification}>
+                <div></div>
+                <div className={styles.Main_Top_Notification}>
+                    Organize all your marketing in one place with marketing
+                    calendar software from CoSchedule.
+                    <span className={styles.Main_Top_Notification_Span}>
+                        Get Started Free
+                    </span>
+                </div>
+                <div className={styles.CrossSvg}>
+                    <CrossSvg />
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/SVG/Cross.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+const CrossSvg = (props) => (
+    <svg
+        className="HelloBar_closeIcon__kkOPe"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 27 27"
+        xmlSpace="preserve"
+        {...props}
+    >
+        <title>{"Close"}</title>
+        <path className="st0" d="m2.6 2.6 21.8 21.8m0-21.8L2.6 24.4" />
+    </svg>
+);
+
+export default CrossSvg;
