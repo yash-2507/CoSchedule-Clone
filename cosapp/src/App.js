1574b0fd2497b750dd141f48db4c980e0b2e983f
AFFAN-AHMAD <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-06-17 07:20:12 +0530

    Card div

--- cosapp/src/App.js
+++ cosapp/src/App.js
@@ -3,13 +3,15 @@ import SuiteNav from './components/suiteNav/SuiteNav';
 import Takecontrol from './components/takecontrol/Takecontrol';
 import CalenderTask from './components/calender/CalenderTask';
 import Close from './components/closeTheOutput/Close';
+import StartComp from './components/startComp/StartComp';
 function App() {
   return (
     <div className="App">
       <SuiteNav />
       <Takecontrol />
       <CalenderTask />
-      <Close/>
+      <Close />
+      <StartComp/>
     </div>
   );
 }
